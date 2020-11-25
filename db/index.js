874d8260d1833667c5410d0df74c5fcfb2a7c7f6
sinonodd <kamal8aly545@gmail.com>
2020-11-25 17:50:26 +0100

    :sparkles: added connection seriously

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const envitonment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('../knexfile.js'); 
 const environmentConfig = knexConfig[environment];
 const knex = require('knex');
