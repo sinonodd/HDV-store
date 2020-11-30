89e4e398e69bcf4b968e799727c1ecee00b7b738
sinonodd <kamal8aly545@gmail.com>
2020-11-30 20:36:43 +0100

    :sparkles: verify token function :smile:

--- auth/utiles.js
+++ auth/utiles.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 function create(user){
-  console.log('this is the user',user);
   return new Promise((resolve,reject) => {
     jwt.sign(user,process.env.TOKEN_SECRET, {expiresIn : '1d'}, (error,token) => {
       if(error) return resolve(error);
@@ -9,4 +8,14 @@ function create(user){
     });
   });
 }
-module.exports = {create};
+
+function verify(token){
+  return new Promise((resolve,reject) => {
+    jwt.verify(token,process.env.TOKEN_SECRET,(error,payload) => {
+      if(error) return resolve(error);
+      resolve(payload);
+    });
+  });
+}
+module.exports = {create,verify};
+
