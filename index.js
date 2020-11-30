7bfe829194cd00c9e3a0a05f9d692fcb6e104c7e
sinonodd <kamal8aly545@gmail.com>
2020-11-30 20:53:56 +0100

    :sparkles: added authheader middlware :smile:

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ require('dotenv').config();
 const app = express();
 const auth = require('./auth')
 const passport = require('passport');
+const {verfify} = require('./auth/utiles');
 
 app.use(volleyball);
 app.use(passport.initialize());
+
+function checkAuthHeaderSetUser(req,res,next) {
+    const authorization = req.get('authorization');
+    console.log(authorization);
+}
 
 app.get('/', (req,res) => {res.json({message:"Hello express" })});
 
 
 function notFound(req,res,next) {
     res.status(404);
-    const error = new Eroor('not Found', req.originUrl);
+    const error = new Error('not Found', req.originUrl);
     next(error);
 }
+// auth route
 app.use('/auth', auth);
 
 function errorHandler(err,req,res,next){
@@ -31,3 +38,6 @@ const port = process.env.PORT ||5000;
 app.listen(port,() => {console.log('listening on port ', port)
 });
 
+app.use(checkAuthHeaderSetUser);
+app.use(notFound);
+app.use(errorHandler);
