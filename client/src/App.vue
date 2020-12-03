41fb42b36c84bc52ec915cc6fd66f41a94a4edcd
sinonodd <kamal8aly545@gmail.com>
2020-12-03 13:37:08 +0100

    :sparkles: init client with navbar and hero :book: :pen:

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/app-nav.vue';
+  export default {
+    components: {
+      AppNav,
+    }
+  };
+</script>
+
+<style lang="scss">
+</style>
